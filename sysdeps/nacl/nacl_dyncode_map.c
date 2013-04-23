a589e0a8bf423917fb62707ef6285119a967a668
Ken Mixter <kmixter@chromium.org>
2013-04-23 11:51:40 -0700

    Prepare for fast loading through mmap'ing text.
    This changes the dynamic text mapping code to attempt PROT_EXEC mapping the text and only if it fails, falling back to read-mapping and using dyncode create.
    
    TEST=newly generated libraries loaded by unmodified sel_ldr.
         dyncode create path is used, code validates, runs fine.
    TEST=newly generated libraries loaded by a modified sel_ldr
         that allows PROT_EXEC mmaping if mmap starts and ends
         on a page align. libraries load.
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3371
    
    Review URL: https://codereview.chromium.org/13598032

--- sysdeps/nacl/nacl_dyncode_map.c
+++ sysdeps/nacl/nacl_dyncode_map.c
@@ -12,14 +12,26 @@
 int nacl_dyncode_map (int fd, void *dest, size_t offset, size_t size)
 {
   size_t alignment_padding = offset & (getpagesize() - 1);
-  uint8_t *mapping = __mmap (NULL, size + alignment_padding,
-                             PROT_READ, MAP_PRIVATE, fd,
-                             offset - alignment_padding);
+  uint8_t *mapping;
+  if (alignment_padding == 0 && (size & (getpagesize() - 1)) == 0) {
+    /* First try mmap using PROT_EXEC directly. */
+    mapping = __mmap(dest, size, PROT_READ | PROT_EXEC,
+                     MAP_PRIVATE | MAP_FIXED, fd, offset);
+    if (mapping == dest) {
+      return 0;
+    } else if (mapping != MAP_FAILED) {
+      /* Mapped to an unexpected location.  Unmap and fall back. */
+      __munmap(mapping, size);
+    }
+  }
+  mapping = __mmap (NULL, size + alignment_padding,
+                    PROT_READ, MAP_PRIVATE, fd,
+                    offset - alignment_padding);
   if (mapping == MAP_FAILED)
     return -1;
   int result = __nacl_dyncode_create (dest, mapping + alignment_padding, size);
 
-  // Tell Valgrind about this mapping.
+  /* Tell Valgrind about this mapping. */
   __nacl_dyncode_map_for_valgrind (dest, size, offset, mapping);
   int munmap_result = __munmap (mapping, size);
   if (result != 0 || munmap_result != 0)
