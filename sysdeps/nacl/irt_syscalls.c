65504607c70fea1d28b4b2f4a550209504664c71
Çağlar Doğan <dogancaglar05@gmail.com>
2020-11-26 02:10:19 +0300

    Added gethostname to  irt_syscalls.c

--- sysdeps/nacl/irt_syscalls.c
+++ sysdeps/nacl/irt_syscalls.c
@@ -22,6 +22,10 @@ static void nacl_irt_exit (int status) {
   }
 }
 
+static int nacl_irt_gethostname (char *name, int len) {
+  return -NACL_SYSCALL (gethostname) (name, len);
+}
+
 static int nacl_irt_gettod (struct timeval *tv) {
   return -NACL_SYSCALL (gettimeofday) (tv, NULL);
 }
@@ -399,6 +403,8 @@ int (*__nacl_irt_open_resource) (const char* file, int *fd);
 int (*__nacl_irt_clock_getres) (clockid_t clk_id, struct timespec *res);
 int (*__nacl_irt_clock_gettime) (clockid_t clk_id, struct timespec *tp);
 
+int (*__nacl_irt_gethostname) (char *name, size_t len);
+
 pid_t (*__nacl_irt_getpid) (void);
 pid_t (*__nacl_irt_getppid) (void);
 int (*__nacl_irt_fork) (void);
@@ -637,6 +643,11 @@ static int nacl_irt_epoll_wait_lind (int epfd, struct epoll_event *events,
         return -rv;
     *count = rv;
     return 0;
+}
+
+static pid_t int (*__nacl_irt_gethostname) (char *name, size_t len)
+{
+    return NACL_SYSCALL (gethostname) ();
 }
 
 /*
@@ -977,6 +988,7 @@ init_irt_table (void)
   __nacl_irt_setsockopt = nacl_irt_setsockopt_lind;
   __nacl_irt_socketpair = nacl_irt_socketpair_lind;
   __nacl_irt_shutdown = nacl_irt_shutdown_lind;
+  __nacl_irt_gethostname = nacl_irt_gethostname;
   __nacl_irt_getpid = nacl_irt_getpid;
   __nacl_irt_getppid = nacl_irt_getppid;
   __nacl_irt_fork = nacl_irt_fork;
