f7ca899f00279bfe402bc4f528377fd80ec1887e
Çağlar Doğan <dogancaglar05@gmail.com>
2020-11-26 03:28:55 +0300

    Update gethostname.c

--- sysdeps/nacl/gethostname.c
+++ sysdeps/nacl/gethostname.c
@@ -9,6 +9,6 @@ int __gethostname(char *name, size_t len)
   return __nacl_irt_gethostname(name, len);
 }
 
-libc_hidden_def (__getpid)
-weak_alias (__getpid, getpid)
-libc_hidden_weak (getpid)
+libc_hidden_def (__gethostname)
+weak_alias (__gethostname, gethostname)
+libc_hidden_weak (gethostname)
