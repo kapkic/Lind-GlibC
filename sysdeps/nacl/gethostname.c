63df5f029f7351d025134718667ab9582c4bc7e0
Çağlar Doğan <dogancaglar05@gmail.com>
2020-11-29 03:19:40 +0300

    Update gethostname.c

--- sysdeps/nacl/gethostname.c
+++ sysdeps/nacl/gethostname.c
@@ -2,10 +2,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#include <stdio.h>
+
 #include <irt_syscalls.h>
 
 int __gethostname(char *name, size_t len)
 {
+  printf("Reached gethostname.c in Lind-GlibC \n");
   return __nacl_irt_gethostname(name, len);
 }
 
