8312a79e8f3af1498f5b9edb5ef354afc3d240e3
Çağlar Doğan <dogancaglar05@gmail.com>
2020-12-21 20:06:28 +0300

    Update pread.c

--- sysdeps/nacl/pread.c
+++ sysdeps/nacl/pread.c
@@ -5,7 +5,7 @@
 #include <irt_syscalls.h>
 
 
-ssize_t __libc_pread (int fd, void *buf, size_t size, off_t offset)
+ssize_t __pread (int fd, void *buf, size_t size, off_t offset)
 {
   size_t nread;
   int result = __nacl_irt_pread (fd, buf, size, &nread, off_t offset);
@@ -15,8 +15,5 @@ ssize_t __libc_pread (int fd, void *buf, size_t size, off_t offset)
   }
   return nread;
 }
-libc_hidden_def (__libc_pread)
-weak_alias (__libc_pread, __pread)
-libc_hidden_weak (__pread)
-weak_alias (__libc_pread, pread)
-strong_alias (__libc_pread, __pread_nocancel)
+
+weak_alias (__pread, pread)
