fafacd1fc96c1c6f94dcb760f9c7842fcbd4d045
Çağlar Doğan <dogancaglar05@gmail.com>
2020-12-22 03:01:41 +0300

    Update pwrite.c

--- sysdeps/nacl/pwrite.c
+++ sysdeps/nacl/pwrite.c
@@ -1,7 +1,6 @@
 #include <errno.h>
 #include <unistd.h>
 #include <sysdep.h>
-#include <sys/types.h>
 
 #include <irt_syscalls.h>
 
@@ -9,7 +8,7 @@
 ssize_t __pwrite(int desc, void const *buf, size_t count, off_t offset)
 {
   size_t nwrite;
-  int result = __nacl_irt_pwrite (desc, buf, count, &nwrite, off_t offset);
+  int result = __nacl_irt_pwrite (desc, buf, count, &nwrite, offset);
   if (result != 0) {
     errno = result;
     return -1;
