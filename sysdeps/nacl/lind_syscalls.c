fcd9a726848732efbde742fe7c6a519697753fc7
kliu513 <62267195+kliu513@users.noreply.github.com>
2021-01-15 04:11:59 +0800

    Test

--- sysdeps/nacl/lind_syscalls.c
+++ sysdeps/nacl/lind_syscalls.c
@@ -483,6 +483,7 @@ int lind_gethostname(char *name, size_t len)
 
 char* lind_getcwd(char* buf, size_t size)
 {
+    printf("lind_syscalls.c")
     LindArg in_args[1] = {{AT_INT, size, 0}};
     LindArg out_args[1] = {{AT_STRING, (char*)buf, size}};
     NACL_SYSCALL(lind_api)(LIND_safe_fs_getcwd, 1, in_args, 1, out_args);
