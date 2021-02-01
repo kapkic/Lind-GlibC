146cd7ea7fb6a6a99f9c17109c4f3db8e65962f9
Çağlar Doğan <dogancaglar05@gmail.com>
2021-02-01 05:32:06 +0300

    Update ioctl.c

--- sysdeps/nacl/ioctl.c
+++ sysdeps/nacl/ioctl.c
@@ -34,6 +34,4 @@ __ioctl (int fd, unsigned long int request, ...) {
 }
 
 libc_hidden_def (__ioctl)
-weak_alias (__ioctl, ioctl)
-strong_alias (__ioctl, __libc_ioctl)
-strong_alias (__ioctl, __ioctl_nocancel)
+strong_alias (__ioctl, ioctl)
