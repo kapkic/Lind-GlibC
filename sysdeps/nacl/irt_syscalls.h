f90e74f1d3ebabccac579acbcfd134b4c050dd12
Çağlar Doğan <dogancaglar05@gmail.com>
2021-02-11 06:33:38 +0300

    Update irt_syscalls.h

--- sysdeps/nacl/irt_syscalls.h
+++ sysdeps/nacl/irt_syscalls.h
@@ -36,6 +36,8 @@ extern int (*__nacl_irt_sysconf) (int name, int *value);
 extern int (*__nacl_irt_mkdir) (const char* pathname, mode_t mode);
 extern int (*__nacl_irt_rmdir) (const char* pathname);
 extern int (*__nacl_irt_chdir) (const char* pathname);
+//extern int (*__nacl_irt_getcwd) (char* buf, size_t size, int *len);
+
 extern int (*__nacl_irt_epoll_create) (int size, int *fd);
 extern int (*__nacl_irt_epoll_create1) (int flags, int *fd);
 extern int (*__nacl_irt_epoll_ctl) (int epfd, int op, int fd,
