f062729304ae60be6e23421de3ddb73dff1dce7b
Çağlar Doğan <dogancaglar05@gmail.com>
2021-02-11 06:37:30 +0300

    Update getcwd.c

--- sysdeps/nacl/getcwd.c
+++ sysdeps/nacl/getcwd.c
@@ -1,8 +1,15 @@
 #include <unistd.h>
-#include <irt_syscalls.h>
+#include <lind_syscalls.h>
 
 char* __getcwd(char* buf, size_t size) {
-    return __nacl_irt_getcwd(buf, size);
+    int error = lind_getcwd(buf, size);
+    if(error==0){
+        return buf;
+    } 
+    else { 
+        __set_errno (error);
+        return NULL;
+    }
 }
 
 weak_alias (__getcwd, getcwd)
