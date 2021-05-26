67b6f18aa710afac58ee517c988275f5fcf9f0ac
Çağlar Doğan <dogancaglar05@gmail.com>
2021-05-26 04:11:05 +0300

    Update nacl_syscalls.h

--- sysdeps/nacl/nacl_syscalls.h
+++ sysdeps/nacl/nacl_syscalls.h
@@ -116,7 +116,7 @@
 #define NACL_sys_wait4                  122
 #define NACL_sys_sigprocmask            123
 #define NACL_sys_lstat                  124
-
+#define NACL_sys_gethostname            125
 #define NACL_sys_pread                  126
 #define NACL_sys_pwrite                 127
 
@@ -229,6 +229,8 @@ typedef int (*TYPE_nacl_waitpid) (int pid, int *stat_loc, int options);
 typedef int (*TYPE_nacl_wait) (int *stat_loc);
 typedef int (*TYPE_nacl_wait4) (pid_t pid, int *wstatus, int options, struct rusage *rusage);
 typedef int (*TYPE_nacl_sigprocmask) (int how, const sigset_t *set, sigset_t *oset);
+
+typedef int (*TYPE_nacl_gethostname) (char *name, size_t len);
 
 typedef int (*TYPE_nacl_pread) (int desc, void *buf, size_t count, off_t offset);
 typedef int (*TYPE_nacl_pwrite) (int desc, const void *buf, size_t count, off_t offset);
