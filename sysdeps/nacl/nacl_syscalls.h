44787e2cdecc0f3c1aaa05a42460c07435600a2a
Çağlar Doğan <dogancaglar05@gmail.com>
2021-05-27 23:41:46 +0300

    Added socket to nacl_syscalls.h

--- sysdeps/nacl/nacl_syscalls.h
+++ sysdeps/nacl/nacl_syscalls.h
@@ -120,6 +120,8 @@
 #define NACL_sys_pread                  126
 #define NACL_sys_pwrite                 127
 
+#define NACL_sys_socket                 136
+
 #define NACL_MAX_SYSCALLS               256
 
 #define NACL_SYSCALL_ADDR(syscall_number) \
@@ -234,5 +236,7 @@ typedef int (*TYPE_nacl_gethostname) (char *name, size_t len);
 
 typedef int (*TYPE_nacl_pread) (int desc, void *buf, size_t count, off_t offset);
 typedef int (*TYPE_nacl_pwrite) (int desc, const void *buf, size_t count, off_t offset);
+
+typedef int (*TYPE_nacl_socket) (int domain, int type, int protocol);
 
 #endif
