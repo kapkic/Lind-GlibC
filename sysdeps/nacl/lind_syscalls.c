ab54705bf69d98505380c887e8fe14263d106fa5
Çağlar Doğan <dogancaglar05@gmail.com>
2021-05-27 08:05:01 +0300

    Update lind_syscalls.c

--- sysdeps/nacl/lind_syscalls.c
+++ sysdeps/nacl/lind_syscalls.c
@@ -473,10 +473,3 @@ void lind_exit(int status)
     LindArg in_args[1] = {{AT_INT, status, 0}};
     NACL_SYSCALL(lind_api)(LIND_sys_exit, 1, in_args, 0, NULL);
 }
-
-int lind_gethostname(char *name, size_t len)
-{
-    LindArg in_args[1] = {{AT_INT, len, 0}};
-    LindArg out_args[1] = {{AT_STRING,(uintptr_t) name, len}};
-    return NACL_SYSCALL(lind_api)(LIND_safe_net_gethostname, 1, in_args, 1, out_args);
-}
