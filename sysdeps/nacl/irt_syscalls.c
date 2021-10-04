181e23b67b717c6e2a9e705acf239493e60a864a
rennergade <nr2185@nyu.edu>
2021-10-04 18:04:35 -0400

    commit fix

--- sysdeps/nacl/irt_syscalls.c
+++ sysdeps/nacl/irt_syscalls.c
@@ -700,11 +700,7 @@ static int nacl_irt_epoll_wait_lind (int epfd, struct epoll_event *events,
 
 static int nacl_irt_gethostname (char *name, size_t len)
 { 
-<<<<<<< HEAD
     return NACL_SYSCALL (gethostname) (name, len);
-=======
-    return lind_gethostname(name, len);
->>>>>>> main
 }
 
 /*
